66dc7dc32f7aa7199f81ddac245a4ea0eefbebb9
Nedgs <nadergwasmi25@gmail.com>
2024-01-24 16:58:01 +0100

    calcul performence

--- piDayFinder_ts/src/index.ts
+++ piDayFinder_ts/src/index.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import * as readlineSync from 'readline-sync';
+import performanceNow from "performance-now";
 
 function findPosition(birthDate: string, filePath: string): number | undefined {
     const fileContent = fs.readFileSync(filePath, 'utf-8');
@@ -13,13 +14,20 @@ function findPosition(birthDate: string, filePath: string): number | undefined {
     }
 }
 
+const startTime = performanceNow();
+
 // Get user input for birth date
 const birthDateToSearch = readlineSync.question('Enter your birth date (MMDD): ');
 
 const foundPosition = findPosition(birthDateToSearch, './../../pi.txt');
+
+const endTime = performanceNow();
 
 if (foundPosition !== undefined) {
     console.log(`The position of ${birthDateToSearch} in the file is: ${foundPosition}`);
 } else {
     console.log(`The number was not found in the file.`);
 }
+
+const executionTime = endTime - startTime;
+console.log(`execution time: ${executionTime} milliseconds`);
