d46505992d2b4ee53382d5fc21081a8fe10c24c3
Nedgs <nadergwasmi25@gmail.com>
2024-01-24 16:23:53 +0100

    [ts_project] jspo

--- piDayFinder_ts/src/index.ts
+++ piDayFinder_ts/src/index.ts
@@ -1,64 +1,25 @@
-import { Big } from 'big.js';
+import * as fs from 'fs';
 import * as readlineSync from 'readline-sync';
 
-// Fonction pour calculer les deux premiers millions de chiffres de pi
-function calculateTwoMillionDigitsOfPi() {
-    let pi = new Big(0);
-    let k = 0;
-    let precision = 1000000;
-    let kMax = precision;
-    let numerator = new Big(1);
-    let denominator = new Big(1);
-    let temp = new Big(0);
-    let sign = 1;
+function findPosition(birthDate: string, filePath: string): number | undefined {
+    const fileContent = fs.readFileSync(filePath, 'utf-8');
+    const position = fileContent.indexOf(birthDate);
 
-    while (k < kMax) {
-        temp = numerator.div(denominator);
-        temp = temp.times(sign);
-        pi = pi.plus(temp);
-        numerator = numerator.plus(2);
-        denominator = denominator.plus(2);
-        sign = sign * -1;
-        k++;
+    if (position !== -1) {
+        // Add 1 because positions start from 0
+        return position + 1;
+    } else {
+        return undefined;
     }
-
-    pi = pi.times(4);
-    return pi;
 }
 
-//afficher la valeur de pi
-const pi = calculateTwoMillionDigitsOfPi();
-console.log(pi);
+// Get user input for birth date
+const birthDateToSearch = readlineSync.question('Enter your birth date (MMDD): ');
 
-// // Fonction pour trouver la position d'une date dans les chiffres de pi
-// function findPositionOfDate(day: string, month: string, piString: string): number {
-//     const dateDigits = `${month}${day}`;
-//     const piSubstring = piString.indexOf(dateDigits);
+const foundPosition = findPosition(birthDateToSearch, './../../pi.txt');
 
-//     if (piSubstring !== -1) {
-//         return piSubstring + 1; // La position commence à 1, pas à 0
-//     } else {
-//         return -1; // La date n'a pas été trouvée
-//     }
-// }
-
-// Calculer les deux premiers millions de chiffres de pi
-// const pi = calculateTwoMillionDigitsOfPi();
-// console.log(pi);
-// const piString = pi.toString();
-
-// // Saisir la date de naissance depuis la console
-// const day = readlineSync.question('Entrez le jour de votre date de naissance (ex. 01): ');
-// const month = readlineSync.question('Entrez le mois de votre date de naissance (ex. 01): ');
-
-// // Trouver la position de la date dans les chiffres de pi
-// const position = findPositionOfDate(day, month, piString);
-
-// // Afficher les résultats
-// if (position !== -1) {
-//     console.log(`Votre date de naissance (${day}-${month}) a été trouvée à la position ${position} dans les deux premiers millions de chiffres de pi.`);
-// } else {
-//     console.log(`Votre date de naissance (${day}-${month}) n'a pas été trouvée dans les deux premiers millions de chiffres de pi.`);
-// }
-
-
+if (foundPosition !== undefined) {
+    console.log(`The position of ${birthDateToSearch} in the file is: ${foundPosition}`);
+} else {
+    console.log(`The number was not found in the file.`);
+}
