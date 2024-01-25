57282f6a43aac91f5ef4e30e138aba4afda404db
Solenn PROVOST <solenn.provost@ausy.fr>
2024-01-25 16:45:13 +0100

    simple research

--- goImpl/main.go
+++ goImpl/main.go
@@ -12,6 +12,7 @@ func main() {
 	content, err := readFileContent("../pi.txt")
 	if err != nil {
 		fmt.Printf("Error during readFileContent execution: %s\n", err)
+		os.Exit(2)
 	}
 	fmt.Println(len(content))
 	var birthdate string
@@ -25,8 +26,12 @@ func main() {
 			fmt.Println("Invalid date format. Please use the format DDMM.")
 		}
 	}
-
-	fmt.Println(birthdate)
+	index := findPattern(content, birthdate)
+	if index != -1 {
+		fmt.Printf("Le motif '%s' a été trouvé à l'index %d.\n", birthdate, index)
+	} else {
+		fmt.Printf("Le motif '%s' n'a pas été trouvé dans le tableau de bytes.\n", birthdate)
+	}
 }
 
 func readFileContent(path string) ([]byte, error) {
@@ -43,4 +48,23 @@ func readFileContent(path string) ([]byte, error) {
 func isValidDateFormat(date string) bool {
 	regex := regexp.MustCompile(`^\d{4}$`)
 	return regex.MatchString(date)
+}
+
+func findPattern(data []byte, pattern string) int {
+	patternBytes := []byte(pattern)
+	patternLen := len(patternBytes)
+
+	for i := 0; i <= len(data)-patternLen; i++ {
+		match := true
+		for j := 0; j < patternLen; j++ {
+			if data[i+j] != patternBytes[j] {
+				match = false
+				break
+			}
+		}
+		if match {
+			return i
+		}
+	}
+	return -1
 }
