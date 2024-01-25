cf3ee4df3ce778e5ed3b06190274d572b3c609f2
Solenn PROVOST <solenn.provost@ausy.fr>
2024-01-25 15:59:00 +0100

    manage error

--- goImpl/main.go
+++ goImpl/main.go
@@ -1,20 +1,46 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"regexp"
 )
 
 func main() {
 
-	content, _ := readFileContent("../pi.txt")
+	content, err := readFileContent("../pi.txt")
+	if err != nil {
+		fmt.Printf("Error during readFileContent execution: %s\n", err)
+	}
 	fmt.Println(len(content))
+	var birthdate string
+	var isValidDate = false
+	for !isValidDate {
+		fmt.Print("Enter your date of birth in the format DDMM: ")
+		fmt.Scanln(&birthdate)
+		isValidDate = isValidDateFormat(birthdate)
+
+		if !isValidDate {
+			fmt.Println("Invalid date format. Please use the format DDMM.")
+		}
+	}
+
+	fmt.Println(birthdate)
 }
 
-func readFileContent(path string) (string, error) {
+func readFileContent(path string) ([]byte, error) {
 	content, err := os.ReadFile(path)
 	if err != nil {
-		return "", err
+		return []byte(""), errors.New("error readFile method")
 	}
-	return string(content), nil
+	if len(content) == 0 {
+		return []byte(""), errors.New("empty content")
+	}
+	return content, nil
+}
+
+func isValidDateFormat(date string) bool {
+	regex := regexp.MustCompile(`^\d{4}$`)
+	return regex.MatchString(date)
 }
